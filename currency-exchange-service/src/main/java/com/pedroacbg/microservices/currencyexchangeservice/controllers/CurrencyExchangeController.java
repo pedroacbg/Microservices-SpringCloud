bec8af4a19530b553ac364c5ed92f474df47a8d6
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-08-30 10:02:39 -0300

    Connecting Currency Exchange Microservice with Zipkin

--- currency-exchange-service/src/main/java/com/pedroacbg/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/pedroacbg/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.pedroacbg.microservices.currencyexchangeservice.controllers;
 
 import com.pedroacbg.microservices.currencyexchangeservice.model.CurrencyExchange;
 import com.pedroacbg.microservices.currencyexchangeservice.repositories.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -21,6 +25,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to){
+        logger.info("retrieveExchangeValue callend with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if(currencyExchange == null){
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
